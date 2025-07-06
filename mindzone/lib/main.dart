3f9b7871079579637e81f6e9f6dbbcf6e2040b81
asaadzx <asaadzeinry@gmail.com>
2025-07-06 12:57:35 +0300

    Integrate Firebase into Mindzone app with necessary configurations and dependencies

--- mindzone/lib/main.dart
+++ mindzone/lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mindzone/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -11,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mindzone App',
       theme: ThemeData(
         // This is the theme of your application.
         //
